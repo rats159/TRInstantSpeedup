823f9fd9232601267e7cbcb04439a58a51accfe0
rats159 <113955400+rats159@users.noreply.github.com>
2024-07-10 15:15:23 -0500

    Add project files.

---
+++ TRInstantSpeedup/Patches/LeaderboardManagerPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace TRInstantSpeedup.Patches;
+[HarmonyPatch(typeof(LeaderboardManager),nameof(LeaderboardManager.StartLeaderboardRun))]
+internal class LeaderboardManagerPatch
+{
+    public static void Postfix(ref LeaderboardManager __instance)
+    {
+        Time.timeScale = 64;
+        __instance.SpeedUp(); //Sets the buttons to be correct
+    }
+}
---
+++ TRInstantSpeedup/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace TRInstantSpeedup;
+
+[BepInPlugin(modGUID,modName,modVersion)]
+public class Root : BaseUnityPlugin
+{
+    const string modGUID = "dev.rats159.tfwr.timed_reset_instant_speedup";
+    const string modName = "Timed Reset Instant Speedup";
+    const string modVersion = "1.0.0";
+
+    public static Root Instance { get; private set; }
+    public readonly Harmony harmony = new(modGUID);
+
+    public void Awake()
+    {
+        Instance = this;
+        harmony.PatchAll();
+    }
+}
